b64d3d444ce3163b40bd18840944a50f77324034
Gonzalo de Bona <gonzalo@cubelizer.com>
2019-03-27 19:26:31 +0100

    update temp/hum

--- climatization/temp_hum.py
+++ climatization/temp_hum.py
@@ -1,31 +1,52 @@
-from Adafruit_Python_DHT import Adafruit_DHT as dht
+import sys
+import Adafruit_DHT
+
 from common import constants
 
 gpioTH = 4
-gpioLedTEMP = 7
+gpioTempHigh = 7
+gpioTempLow =
+gpioHumHigh =
+gpioHumLow =
+sensor = 22
 
 
 def eval_climatization(
-	UMBRAL_TEMP_L, 
-	UMBRAL_TEMP_H, 
-	UMBRAL_HUM_L, 
-	UMBRAL_HUM_H
+        UMBRAL_TEMP_L,
+        UMBRAL_TEMP_H,
+        UMBRAL_HUM_L,
+        UMBRAL_HUM_H
 ):
-	humidity,temperature = dht.read_retry(dht.DHT22, gpioTH)
-	print ('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    while 1:
+        humidity, temperature = Adafruit_DHT.read_retry(sensor, gpioTH)
+        if humidity is not None and temperature is not None:
+            print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+        else:
+            print('Failed to get reading. Try again!')
+            sys.exit(1)
 
-	# Llamar a la funcion que activa o desactiva los ventiladores
-	if UMBRAL_TEMP_L < temperature < UMBRAL_TEMP_H:
-		# Realizar accion
-		gpioLedTEMP = LOW
-	else:
-		# Realizar accion
-		gpioLedTEMP = HIGH
+        # Llamar a la funcion que activa o desactiva los ventiladores
+        if UMBRAL_TEMP_L > temperature:
+            # Realizar accion
+            gpioTempLow = HIGH
+            gpioTempHigh = LOW
+        elif temperature > UMBRAL_TEMP_H:
+            # Realizar accion
+            gpioTempLow = LOW
+            gpioTempHigh = HIGH
+        else:
+        	gpioTempLow = LOW
+            gpioTempHigh = LOW
 
-	# Llamar a la funcion que activa la bomba o el ventilador de humedad
-	if UMBRAL_HUM_L < humidity < UMBRAL_HUM_H:
-		# Realizar accion
-		gpioLedHUM = LOW
-	else:
-		# Realizar accion
-		gpioLedHUM = HIGH
+        # Llamar a la funcion que activa la bomba o el ventilador de humedad
+        if UMBRAL_HUM_L > humidity:
+            # Realizar accion
+            gpioHumLow = HIGH
+            gpioHumHigh = LOW
+        elif temperature > UMBRAL_HUM_H:
+            # Realizar accion
+            gpioHumLow = LOW
+            gpioHumHigh = HIGH
+        else:
+        	gpioHumLow = LOW
+            gpioHumHigh = LOW
--- project.py
+++ project.py
@@ -12,26 +12,36 @@ def read_json_file(dataset_path):
 
 
 def get_params(type_weed, params):
-	for param in params:
-		if param == type_weed:
-			temp_umbral = params[param]['temperature_umbral']
-			hum_umbral = params[param]['humidity_umbral']
-	return temp_umbral, hum_umbral
+    for param in params:
+        if param == type_weed:
+            temp_umbral = params[param]['temperature_umbral']
+            hum_umbral = params[param]['humidity_umbral']
+    return temp_umbral, hum_umbral
+
 
 def handler(type_weed: str) -> None:
-	params = read_json_file('config.json')
-	temp_umbral, hum_umbral = get_params(type_weed, params) # primer parametro HIGh temp_umbral[0] = umbral por arriba
-	
+    params = read_json_file('config.json')
+    # primer parametro HIGh temp_umbral[0] = umbral por arriba
+    temp_umbral, hum_umbral = get_params(type_weed, params)
+    print('Temp min:', temp_umbral[0],
+          'Temp max:', temp_umbral[1])
+
+    print('Hum min:', hum_umbral[0],
+          'Hum max:', hum_umbral[1])
+
+    # Read Temp/Hum
+    # humidity, temp = read_temphum()
+    # Eval Temp/Hum
 
 if __name__ == '__main__':
 
-	parser = argparse.ArgumentParser(
-    	prog='plan',
-    	description='tipo de planta',
-	)
-	parser.add_argument('plt', help='tipo de planta')
+    parser = argparse.ArgumentParser(
+        prog='plan',
+        description='tipo de planta',
+    )
+    parser.add_argument('plt', help='tipo de planta')
 
-	args = parser.parse_args()
-	handler(
-	    args.plt,
-	)
+    args = parser.parse_args()
+    handler(
+        args.plt,
+    )
